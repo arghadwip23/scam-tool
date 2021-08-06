e39fcf635a0fde649ca1c6750aaa5fa98e87bed3
arghadwip23 <arghadwip23@gmail.com>
2021-08-06 18:51:09 +0530

    abc

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 console.log('Hello World!');
+
+//🔥
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyClL7pE-1kTSr2ouTpFHnwdvhW36rLgX2s",
+    authDomain: "jnvcob.firebaseapp.com",
+    databaseURL: "https://jnvcob.firebaseio.com",
+    projectId: "jnvcob",
+    storageBucket: "jnvcob.appspot.com",
+    messagingSenderId: "69566502508",
+    appId: "1:69566502508:web:be3e4ad98d78924cb7aa49",
+    measurementId: "G-BH23ZFD2KB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+
+var db = firebase.firestore(); 
+
+
+
 var con = document.getElementById('abc');
 var cok = document.cookie;
 console.log(cok);
@@ -36,9 +60,14 @@ if(input.value.length>=10){
 
 
 btn.addEventListener("click" , () =>{
- var number = document.getElementById("number").value;
-if (regx.test(number)) {
-  Swal.fire({
+document.getElementById("modal").style.display="flex";
+ db.collection("user").add({
+     Datea:  new Date().toLocaleDateString(),
+     Time:   new Date().toLocaleTimeString(),
+     Numbers: input.value
+   }).then((docRef) => {
+   document.getElementById("modal").style.display="none";
+     Swal.fire({
     position: 'center',
     icon: 'success',
     title: 'transection is successful',
@@ -47,35 +76,25 @@ if (regx.test(number)) {
   document.cookie = new Date().toLocaleDateString()+","+new Date().toLocaleTimeString()+","+input.value;
   con.innerHTML=`<h2 class='text-success'>your transection is successful</h2>` ;
 
-}else{
- document.getElementById("number").style.borderColor="red";
-}
+   }).catch((error) => {
+     console.error("Error adding document: ", error);
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        title: 'transection is  unsuccessful',
+        showConfirmButton: true
+      });
+   });
   
   
 });
 }else{
+
+
+
   var data = cok.split(',');
   con.innerHTML=`<h2 class='text-success'>Your prize money has been sent to <span class="text-primary">${data[2]}</span> on <span class='text-primary'>${data[0]}</span>(mm/dd/yy) at <span class='text-primary'>${data[1]}</span> </h2>`
   
   
 }
 
-
-
-
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
